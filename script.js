890298c652e7b0604f1e0a3a06a58acde753809e
Charly Wolf <cardan1990@hotmail.com>
2021-10-11 10:46:59 +0200

    Added message parts

---
+++ script.js
@@ -0,0 +1,5 @@
+const greetings = ["Hello", "Hi!", "Hey!", "What's up?"];
+const readyness = ["on your way", "almost ready", "ready", "more than ready"];
+const level = ["rookie", "good coder", "master", "hacker"];
+const language = ["Python", "JavaScript", "Java", "C#"];
+
